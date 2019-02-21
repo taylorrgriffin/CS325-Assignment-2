04d7cf17c6c53d110f78ea7e5ac8a974e0ee2e98
Drew S. Ortega <DrewSOrtega@gmail.com>
2019-02-20 19:28:53 -0800

    finished traceback parsing

--- align_sequences.py
+++ align_sequences.py
@@ -2,21 +2,26 @@ import os
 import sys
 import helper
 
-def opt_align(m,n,seq1,seq2,matrix):
+
+def opt_align(m, n, seq1, seq2, matrix):
     opt_results = []
-    opt_results.append([(0,0)])
+    opt_results.append([(0, 0)])
     for j in range(1, n+1):
-        opt_results[0].append((opt_results[0][j-1][0]+helper.cost('-', seq2[j-1], matrix),3))
+        opt_results[0].append(
+            (opt_results[0][j-1][0]+helper.cost('-', seq2[j-1], matrix), 3))
     for i in range(1, m+1):
-        opt_results.append([(opt_results[i-1][0][0]+helper.cost(seq1[i-1], '-', matrix),2)])
+        opt_results.append(
+            [(opt_results[i-1][0][0]+helper.cost(seq1[i-1], '-', matrix), 2)])
         for j in range(1, n+1):
             opt_results[i].append(min(
                 [
-                    (opt_results[i-1][j-1][0]+helper.cost(seq1[i-1],seq2[j-1],matrix),1),
-                    (opt_results[i-1][j][0]+helper.cost(seq1[i-1],'-',matrix),2),
-                    (opt_results[i][j-1][0]+helper.cost('-',seq2[j-1],matrix),3)
-                ]
-            ,key=lambda tup: tup[0]))
+                    (opt_results[i-1][j-1][0] +
+                     helper.cost(seq1[i-1], seq2[j-1], matrix), 1),
+                    (opt_results[i-1][j][0] +
+                     helper.cost(seq1[i-1], '-', matrix), 2),
+                    (opt_results[i][j-1][0] +
+                     helper.cost('-', seq2[j-1], matrix), 3)
+                ], key=lambda tup: tup[0]))
     x = opt_results[m][n][1]
     i = m
     j = n
@@ -24,17 +29,25 @@ def opt_align(m,n,seq1,seq2,matrix):
     opt_seq2 = ""
     while (x > 0):
         if (x == 1 or x == 3):
-            opt_seq2 = seq2[j-1] + opt_seq2
-            j = j - 1
+            if(j <= 0):
+                opt_seq2 = '-' + opt_seq2
+            else:
+                opt_seq2 = seq2[j-1] + opt_seq2
+                j = j - 1
         if (x == 1 or x == 2):
-            opt_seq1 = seq1[i-1] + opt_seq1
-            i = i -1
+            if(i <= 0):
+                opt_seq1 = '-' + opt_seq1
+            else:
+                opt_seq1 = seq1[i-1] + opt_seq1
+                i = i - 1
         if (x == 2):
-            opt_seq2 = '-' + opt_seq2 
+            opt_seq2 = '-' + opt_seq2
         if (x == 3):
             opt_seq1 = "-" + opt_seq1
-        
-    return(opt_seq1+','+opt_seq2+':'+opt_results[m][n][0])
+        x = opt_results[i][j][1]
+
+    return(opt_seq1+','+opt_seq2+':'+str(opt_results[m][n][0]))
+
 
 input_matrix = sys.argv[1]
 input_sequence = sys.argv[2]
@@ -47,5 +60,4 @@ for pair in pairs:
     seq2 = pair[1]
     m = len(seq1)
     n = len(seq2)
-    print(opt_align(m,n,seq1,seq2,matrix))
-
+    print(opt_align(m, n, seq1, seq2, matrix))
