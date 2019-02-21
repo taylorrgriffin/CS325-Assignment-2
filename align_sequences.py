d906af902b7715218b7dbcc005bfe7e8d87df389
Drew S. Ortega <DrewSOrtega@gmail.com>
2019-02-20 20:20:34 -0800

    added comments

--- align_sequences.py
+++ align_sequences.py
@@ -4,49 +4,60 @@ import helper
 
 
 def opt_align(m, n, seq1, seq2, matrix):
+    # Results: 2, 2-D list/table with m rows and n columns.
+    #   opt_results[i][j] = a tuple.
+    #       First element  [0] = Calculated cost given length of i and j.
+    #       Second element [1] = Traceback direction.
+    #           Traceback list legend:
+    #               0 - no traceback direction. this only occurs at the i=j=0.
+    #               1 - go diaganally, i-1 & j-1. No '-' is inserted.
+    #               2 - go up a row, i-1 & j. Insert a '-' on sequence 2.
+    #               3 - go left a column, i & j-1. Insert a '-' on sequence 1.
     opt_results = []
+
+    # initialize top left of table, no traceback & no cost.
     opt_results.append([(0, 0)])
+
+    # initialize first row base cases.
     for j in range(1, n+1):
         opt_results[0].append(
             (opt_results[0][j-1][0]+helper.cost('-', seq2[j-1], matrix), 3))
+
+    # do DP
     for i in range(1, m+1):
+        # first element of j for j=0 means no length, so a '-' will always be inserted.
+        # inserts a list of a tuple.
         opt_results.append(
             [(opt_results[i-1][0][0]+helper.cost(seq1[i-1], '-', matrix), 2)])
-        for j in range(1, n+1):
-            opt_results[i].append(min(
-                [
-                    (opt_results[i-1][j-1][0] +
-                     helper.cost(seq1[i-1], seq2[j-1], matrix), 1),
-                    (opt_results[i-1][j][0] +
-                     helper.cost(seq1[i-1], '-', matrix), 2),
-                    (opt_results[i][j-1][0] +
-                     helper.cost('-', seq2[j-1], matrix), 3)
-                ], key=lambda tup: tup[0]))
-    x = opt_results[m][n][1]
-    i = m
-    j = n
-    opt_seq1 = ""
-    opt_seq2 = ""
-    while (x > 0):
-        if (x == 1 or x == 3):
-            if(j <= 0):
-                opt_seq2 = '-' + opt_seq2
-            else:
-                opt_seq2 = seq2[j-1] + opt_seq2
-                j = j - 1
-        if (x == 1 or x == 2):
-            if(i <= 0):
-                opt_seq1 = '-' + opt_seq1
-            else:
-                opt_seq1 = seq1[i-1] + opt_seq1
-                i = i - 1
-        if (x == 2):
-            opt_seq2 = '-' + opt_seq2
-        if (x == 3):
-            opt_seq1 = "-" + opt_seq1
-        x = opt_results[i][j][1]
 
-    return(opt_seq1+','+opt_seq2+':'+str(opt_results[m][n][0]))
+        for j in range(1, n+1):
+
+            # set the jth value in the ith row to be the minimum of the following:
+            opt_results[i].append(
+                min(
+                    [
+                        # align i with j
+                        (opt_results[i-1][j-1][0] +
+                         helper.cost(seq1[i-1], seq2[j-1], matrix), 1),
+                        # insert a '-' on seq2
+                        (opt_results[i-1][j][0] +
+                         helper.cost(seq1[i-1], '-', matrix), 2),
+                        # insert a '-' on seq1
+                        (opt_results[i][j-1][0] +
+                         helper.cost('-', seq2[j-1], matrix), 3)
+                    ],
+                    # sort by the first value of the tuple, the cost
+                    key=lambda tup: tup[0]
+                )
+            )
+
+    # Do traceback. Gets a tuple.
+    # opt_seqs[0] = opt_seq1
+    # opt_seqs[1] = opt_seq2
+    opt_seqs = helper.traceback_sequences(m, n, seq1, seq2, opt_results)
+
+    # Return resulting string
+    return(opt_seqs[0]+','+opt_seqs[1]+':'+str(opt_results[m][n][0]))
 
 
 input_matrix = sys.argv[1]
--- helper.py
+++ helper.py
@@ -3,26 +3,29 @@ import os
 import csv
 import random
 
-def cost(s1,s2,costMatrix):
+
+def cost(s1, s2, costMatrix):  # takes in two characters and finds their cost.
     return(int(next(item for item in costMatrix if item['key'] == s1)[s2]))
+
 
 def create_random_sequences(size):
     # create a sequence pair of a given length
     seq1 = ""
     seq2 = ""
     generated = 0
-    choices = ['G','A','T','C']
+    choices = ['G', 'A', 'T', 'C']
     while (generated < size):
-        seq1 += choices[random.randint(0,3)]
-        seq2 += choices[random.randint(0,3)]
+        seq1 += choices[random.randint(0, 3)]
+        seq2 += choices[random.randint(0, 3)]
         generated = generated + 1
     return(seq1+','+seq2)
+
 
 def generate_sequences():
     # make sure directory exists for generated sequences
     if not os.path.exists("random_sequences"):
         os.makedirs("random_sequences")
-    sizes = [500,1000,2000,4000,5000]
+    sizes = [500, 1000, 2000, 4000, 5000]
     file_prefix = "random_sequences/input_"
     file_extension = ".txt"
     # generate 10 random pairs of sequences for each size
@@ -34,11 +37,13 @@ def generate_sequences():
             generated = generated + 1
         output.close()
 
+
 def parse_sequences(file_path):
     with open(file_path, 'r') as f:
         reader = csv.reader(f)
         lines = list(reader)
     return(lines)
+
 
 def parse_weight_matrix(file_path):
     # parse csv into lines
@@ -55,3 +60,39 @@ def parse_weight_matrix(file_path):
                 weights_row[dst[0]] = dst[i]
             weights.append(weights_row)
     return(weights)
+
+
+# traceback to find optimal sequences
+def traceback_sequences(m, n, seq1, seq2, opt_results):
+    x = opt_results[m][n][1]
+    i = m
+    j = n
+    opt_seq1 = ""
+    opt_seq2 = ""
+    while (x > 0):
+        # 1 or 3 both do column
+        if (x == 1 or x == 3):
+            # account for base cases to not go out of range
+            if(j <= 0):
+                opt_seq2 = '-' + opt_seq2
+            # if not base case, align
+            else:
+                opt_seq2 = seq2[j-1] + opt_seq2
+                j = j - 1
+        # 1 or 2 both do row
+        if (x == 1 or x == 2):
+            # account for base cases to not go out of range
+            if(i <= 0):
+                opt_seq1 = '-' + opt_seq1
+            # if not base case, align
+            else:
+                opt_seq1 = seq1[i-1] + opt_seq1
+                i = i - 1
+        if (x == 2):
+            opt_seq2 = '-' + opt_seq2
+        if (x == 3):
+            opt_seq1 = "-" + opt_seq1
+        # update x
+        x = opt_results[i][j][1]
+    # return tuple of results
+    return (opt_seq1, opt_seq2)
